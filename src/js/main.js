e66ac7a093cd732cd21cbdf98da53f7c6f73d358
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-21 10:52:26 +0100

    Render one card

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('>> Ready :)');
+const characterUl = document.querySelector('.js_characterUl');
+
+
+characterUl.innerHTML =
+
+    `<li class="character__card">
+
+        <div class="character__img"> 
+            <img src="https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png"
+            alt="Achilles">
+        </div>
+
+        <p class="character__name">Achilles</p>
+
+    </li>`;
+
