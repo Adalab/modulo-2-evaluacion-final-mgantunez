50efed468bb44c8caf225f591df712ae2b87c69b
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-21 10:59:16 +0100

    Create const for render one card

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,21 @@
 const characterUl = document.querySelector('.js_characterUl');
 
 
-characterUl.innerHTML =
+const renderOneCard = () => {
 
-    `<li class="character__card">
+    characterUl.innerHTML =
+
+        `<li class="character__card">
 
         <div class="character__img"> 
-            <img src="https://static.wikia.nocookie.net/disney/images/d/d3/Vlcsnap-2015-05-06-23h04m15s601.png"
-            alt="Achilles">
+            <img src="${imageUrl}"
+            alt="${name}">
         </div>
 
-        <p class="character__name">Achilles</p>
+        <p class="character__name">Imagen de ${name}</p>
 
     </li>`;
 
+}
+
+renderOneCard();
