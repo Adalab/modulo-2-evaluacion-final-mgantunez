7922bd1dea82a66cb2857b5bbadbb381c4b575cb
María García Antúnez <maria.garcia.antunez@gmail.com>
2024-12-21 12:41:54 +0100

    Fix images in cards

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,44 @@
 
 const characterUl = document.querySelector('.js_characterUl');
 
+
 let allCharactersCards = [];
 
+
+const getVisibleImageUrl = (imageUrl) => {
+
+    if (!imageUrl) {
+
+        return 'https://placehold.co/400x400/ffffff/555555?text=Disney';
+    }
+
+    return imageUrl;
+
+}
+
+
 const renderOneCard = (cardObj) => {
+
+    debugger;
+
+    const imageUrl = getVisibleImageUrl(cardObj.imageUrl);
 
     const html =
 
         `<li class="character__card">
 
-        <div class="character__img"> 
-            <img src="${cardObj.imageUrl}"
+        <div> 
+            <img class="character__img" src="${imageUrl}"
             alt="Imagen de ${cardObj.name}">
         </div>
-
         <p class="character__name">${cardObj.name}</p>
-
+        <i class="fa-regular fa-heart"></i>
     </li>`;
 
     return html;
 
 }
+
 
 
 const renderAllCards = () => {
